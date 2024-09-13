55528f65d04ae2edfdc1cf61535ed411566b257e
amit.chauhan <amit.c@zyapaar.com>
2024-09-13 17:23:06 +0530

    video 5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { AddEmployeeComponent } from './component/add-employee/add-employee.component';
 import { EmployeeListComponent } from './component/employee-list/employee-list.component';
 import { DataBindingComponent } from './component/data-binding/data-binding.component';
+import { StructuralDirComponent } from './component/directive/structural-dir/structural-dir.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,AddEmployeeComponent,EmployeeListComponent,DataBindingComponent],
+  imports: [RouterOutlet,AddEmployeeComponent,EmployeeListComponent,DataBindingComponent,StructuralDirComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
