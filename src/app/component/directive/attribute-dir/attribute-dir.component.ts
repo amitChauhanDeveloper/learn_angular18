d535fc65cae72159b09c9373cf306edee062de2f
amit.chauhan <amit.c@zyapaar.com>
2024-09-13 17:29:47 +0530

    video 6

---
+++ src/app/component/directive/attribute-dir/attribute-dir.component.ts
@@ -0,0 +1,52 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-attribute-dir',
+  standalone: true,
+  imports: [CommonModule,FormsModule],
+  templateUrl: './attribute-dir.component.html',
+  styleUrl: './attribute-dir.component.css'
+})
+export class AttributeDirComponent {
+
+  isDiv1Visable: boolean = true;
+  isDiv2Visable: boolean = true;
+  num1: String = '';
+  num2: String = '';
+  isActive: boolean = false;
+  seletedState: String = 'Bihar';
+
+  cityArray: string[] = ['Select City','Ahmedabad','Gandhinagar','Kalol','Mahesana']
+
+  studentList: any[]=[
+    {studentId: 1, name: 'Amit', city: 'Kalol', isActive: true},
+    {studentId: 2, name: 'Ajay', city: 'Mahesana', isActive: false},
+    {studentId: 3, name: 'Naitik', city: 'Ahmedabad', isActive: true},
+    {studentId: 4, name: 'Mohan', city: 'Chandkheda', isActive: false},
+    {studentId: 5, name: 'Sanjay', city: 'Vadodara', isActive: false},
+    {studentId: 6, name: 'Manan', city: 'Surat', isActive: false}
+  ]
+
+  showDiv1(){
+    this.isDiv1Visable = true;
+  }
+
+  hideDiv1(){
+    this.isDiv1Visable = false;
+  }
+
+  toggleDiv2(){
+    // short code
+    this.isDiv2Visable = !this.isDiv2Visable;
+
+    // long code
+    // if (this.isDiv2Visable == true) {
+    //   this.isDiv2Visable = false;
+    // }else{
+    //   this.isDiv2Visable = true;
+    // }
+  }
+
+}
