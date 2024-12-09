dcf00ed227ffc4b69397ccfe2943a6157e24f739
amit chauhan <email@tender247.com>
2024-12-09 16:06:52 +0530

    add another project personal finance tracker

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [FormsModule],
-  templateUrl: './home.component.html'
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
   selectedColor: string = '#ff0000';
+
+  router = inject(Router);
+
+  // Use this for external navigation
+  redirectOnPersonalFinanceTrackerPage(): void {
+    window.location.href = 'https://personal-finance-tracker-three-sigma.vercel.app/';
+  }
 
 }
